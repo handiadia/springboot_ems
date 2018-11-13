0a44d4930c2a2244040d574de21cd36671a088d7
root <17671355464@163.com>
2018-11-13 16:17:43 +0800

    this is springboot_ems

--- src/main/java/com/baizhi/App.java
+++ src/main/java/com/baizhi/App.java
@@ -6,11 +6,13 @@ import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.HttpMessageConverter;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @MapperScan("com.baizhi.dao")
 public class App {
     public static void main(String[] args) {
