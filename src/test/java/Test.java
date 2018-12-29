dcfd4071b1671e914992d86a7c6d65d4ea404246
handiadia <44966614+handiadia@users.noreply.github.com>
2018-12-29 21:35:11 +0800

    Update Test.java

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -2,8 +2,6 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("333");
-        System.out.println("这是修改后的代码");
-        System.out.println("这是修改后的代码");
     }
 
 }
