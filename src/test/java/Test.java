829370d9fbf180213f5936e533410e906e1a890d
handiadia <44966614+handiadia@users.noreply.github.com>
2018-12-29 22:26:36 +0800

    Update Test.java

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,10 +1,6 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("999");
-        System.out.println("999");
-        System.out.println("999");
-        System.out.println("999");
-        System.out.println("999");
+        System.out.println("测试");
     }
 }
