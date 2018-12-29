06837ffb477b2378a4f4f2f0e6a20cd42290450e
root <17671355464@163.com>
2018-12-29 21:36:18 +0800

    测试冲突

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -2,8 +2,9 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("333");
-        System.out.println("这是修改后的代码");
-        System.out.println("这是修改后的代码");
+        System.out.println("这是修改后的代码1");
+        System.out.println("这是修改后的代码2");
+        System.out.println("这是修改后的代码3");
     }
 
 }
